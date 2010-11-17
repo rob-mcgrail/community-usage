6665269fbc54381afba5e3b80f16496a204815b9
rob <rob@rob-laptop.(none)>
2010-11-17 15:09:55 +1300

    tidied up

--- lib/community_usage/site.rb
+++ lib/community_usage/site.rb
@@ -2,17 +2,18 @@ class Site
 	attr_reader :name, :code, :profile
 	attr_accessor :segment, :nz
 	include Stats
-	
+
 	def initialize(name, code, community, total, slice)
 		@name = name
 		@code = code
 		@community = community
 		@total = total
 		@slice = slice
-		@nz = nil	
+		@nz = nil
 	  @profile = Garb::Profile.first(@code)
 	end
-	
+
+	#
 	#
 	# Methods for sorting through the list of sites, like:
 	#
@@ -20,25 +21,26 @@ class Site
 	#   @total_for_community += site.visits if site.is_community?
 	# end
 	#
-	
+
 	def community_page?
 		@community
 	end
   alias :communities? :community_page?
-	
+
 	def is_segment?
 		@slice[:total]
 	end
   alias :slice? :is_segment?
-  
+
   def is_for_total?
     @total
   end
   alias :total? :is_for_total?
-	
+
 	def is_nz?
 	  @nz
 	end
 	alias :nz? :is_nz?
-	
+
 end
+
