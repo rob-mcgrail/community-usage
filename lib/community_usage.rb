453257f43d6b13414f8099254a4896a421fb5da3
robomc <rob.mcgrail@gmail.com>
2011-01-06 11:29:17 +1300

    changed requires

--- lib/community_usage.rb
+++ lib/community_usage.rb
@@ -1,9 +1,11 @@
 require 'rubygems'
-require 'garb'            # gem install garb -v 0.8.0
-require 'active_support'  # gem install rails - also required by garb, may not be necessary
+require 'garb'          # gem install garb -v 0.8.0
+require 'active_support'
+                        # I also use active_support (so does garb, so above command should install it)
 
 $:.unshift File.expand_path(File.dirname(__FILE__))
 
+require 'community_usage/date'
 require 'community_usage/math'
 require 'community_usage/interface'
 require 'community_usage/stats'
