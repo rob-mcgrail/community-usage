9689cb3bde3d41f008a8c0e6e4f00085e7780aee
robomc <rob.mcgrail@gmail.com>
2011-04-01 11:44:05 +1300

    removed meaningless nonsense comment

--- lib/community_usage.rb
+++ lib/community_usage.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'garb'          # gem install garb -v 0.8.0
 require 'active_support'
-                        # I also use active_support (so does garb, so above command should install it)
+
 
 $:.unshift File.expand_path(File.dirname(__FILE__))
 
